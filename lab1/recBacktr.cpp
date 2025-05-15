a3776e8c5c4d3150f749406a6076b0963cbd10d9
Niamon <56646734+Niamon@users.noreply.github.com>
2025-05-15 14:35:13 +0300

    Adding 1,3 labs

---
+++ lab1/recBacktr.cpp
@@ -0,0 +1,113 @@
+﻿#include <iostream>
+using namespace std;
+
+const int MAX_N = 20;
+
+struct Square {
+    int x = 0, y = 0, w = 0;
+};
+
+
+void dfs(int n, int m, int heights[], int cnt, int& bestCnt,
+    Square currentPath[], Square bestPath[], int& currentLen) {
+
+    if (cnt > bestCnt) return;
+
+// Поиск самой низкой колонки
+    int left = 0, minHeight = m;
+    for (int i = 0; i < n; i++) {
+        if (heights[i] < minHeight) {
+            minHeight = heights[i];
+            left = i;
+        }
+    }
+
+// Если всё покрыто
+    if (minHeight == m) {
+        if (cnt < bestCnt) {
+            bestCnt = cnt;
+            countWays = 1;
+            for (int i = 0; i < cnt; ++i) {
+                bestPath[i] = currentPath[i];
+            }
+            bestPathSet = true;
+        }
+        else if (cnt == bestCnt) {
+            countWays++;
+            if (!bestPathSet) {
+                for (int i = 0; i < cnt; ++i) {
+                    bestPath[i] = currentPath[i];
+                }
+                bestPathSet = true;
+            }
+        }
+        return;
+    }
+// Поиск свободной ширины справа
+    int right = left;
+    while (right < n && heights[right] == minHeight && (right - left + minHeight) < m) {
+        ++right;
+    }
+
+    int maxSize = right - left;
+    if (m - minHeight < maxSize) maxSize = m - minHeight;
+
+    for (int size = maxSize; size >= 1; size--) {
+        if (cnt + 1 > bestCnt) continue;
+        if (size >= n || size >= m) continue;
+
+        
+       /* cout << string(cnt * 2, ' ') << "[DFS level " << cnt << "] Пытаемся поставить квадрат "
+            << size << "x" << size << " в точку (" << left + 1 << ", " << minHeight + 1 << ")" << endl;*/
+
+        for (int i = left; i < left + size; ++i)
+            heights[i] += size;
+
+       
+    /*    cout << string(cnt * 2, ' ') << "Высоты после установки: [";
+        for (int i = 0; i < n; ++i) cout << heights[i] << (i < n - 1 ? ", " : "");
+        cout << "]" << endl;*/
+
+        currentPath[currentLen++] = { left + 1, minHeight + 1, size };
+        dfs(n, m, heights, cnt + 1, bestCnt, currentPath, bestPath, currentLen);
+        currentLen--;
+
+        for (int i = left; i < left + size; ++i)
+            heights[i] -= size;
+
+       
+     /*   cout << string(cnt * 2, ' ') << "Откат после квадрата " << size << "x" << size
+            << " в точке (" << left + 1 << ", " << minHeight + 1 << ")" << endl;*/
+    }
+}
+
+
+int tilingRectangle(int n, int m) {
+    if (n > m) swap(n, m);
+
+    int heights[MAX_N] = { 0 };
+    Square currentPath[MAX_N * MAX_N];
+    Square bestPath[MAX_N * MAX_N];
+    int currentLen = 0, bestLen = n * m;
+
+    countWays = 0;
+    bestPathSet = false;
+
+    dfs(n, m, heights, 0, bestLen, currentPath, bestPath, currentLen);
+
+    cout << "Минимальное количество квадратов: " << bestLen << endl;
+    for (int i = 0; i < bestLen; ++i) {
+        cout << bestPath[i].x << " " << bestPath[i].y << " " << bestPath[i].w << endl;
+    }
+    cout << "Количество способов заполнения прямоугольника минимльным количеством квадратов: " << countWays << endl;
+
+    return bestLen;
+}
+
+int main() {
+    setlocale(LC_ALL, "Russian");
+    int n, m;
+    cin >> n >> m;
+    tilingRectangle(n, m);
+    return 0;
+}
---
+++ lab3/src/RedDistance.cpp
@@ -0,0 +1,279 @@
+﻿#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+
+struct Cell {
+    int cost;
+    char operation;
+};
+
+
+/*vector<vector<Cell>> build_dp(const string& A, const string& B, int cost_replace, int cost_insert, int cost_delete) {
+    int m = A.length();
+    int n = B.length();
+    vector<vector<Cell>> dp(m + 1, vector<Cell>(n + 1));
+
+    dp[0][0] = { 0, 'M' };
+    for (int i = 1; i <= m; ++i)
+        dp[i][0] = { dp[i - 1][0].cost + cost_delete, 'D' };
+    for (int j = 1; j <= n; ++j)
+        dp[0][j] = { dp[0][j - 1].cost + cost_insert, 'I' };
+
+    for (int i = 1; i <= m; ++i) {
+        for (int j = 1; j <= n; ++j) {
+            if (A[i - 1] == B[j - 1]) {
+                dp[i][j] = { dp[i - 1][j - 1].cost, 'M' };
+            }
+            else {
+                int replace_cost = dp[i - 1][j - 1].cost + cost_replace;
+                int insert_cost = dp[i][j - 1].cost + cost_insert;
+                int delete_cost = dp[i - 1][j].cost + cost_delete;
+
+                if (replace_cost <= insert_cost && replace_cost <= delete_cost) {
+                    dp[i][j] = { replace_cost, 'R' };
+                }
+                else if (insert_cost <= replace_cost && insert_cost <= delete_cost) {
+                    dp[i][j] = { insert_cost, 'I' };
+                }
+                else {
+                    dp[i][j] = { delete_cost, 'D' };
+                }
+            }
+        }
+    }
+
+    return dp;
+}
+
+void find_all_paths(const vector<vector<Cell>>& dp, const string& A, const string& B,
+    int i, int j, string current, vector<string>& all_paths,
+    int cost_replace, int cost_insert, int cost_delete) {
+    if (i == 0 && j == 0) {
+        reverse(current.begin(), current.end());
+        all_paths.push_back(current);
+        return;
+    }
+
+    if (i > 0 && j > 0) {
+        if (A[i - 1] == B[j - 1] && dp[i][j].cost == dp[i - 1][j - 1].cost) {
+            find_all_paths(dp, A, B, i - 1, j - 1, current + 'M', all_paths, cost_replace, cost_insert, cost_delete);
+        }
+        if (A[i - 1] != B[j - 1] && dp[i][j].cost == dp[i - 1][j - 1].cost + cost_replace) {
+            find_all_paths(dp, A, B, i - 1, j - 1, current + 'R', all_paths, cost_replace, cost_insert, cost_delete);
+        }
+    }
+    if (j > 0 && dp[i][j].cost == dp[i][j - 1].cost + cost_insert) {
+        find_all_paths(dp, A, B, i, j - 1, current + 'I', all_paths, cost_replace, cost_insert, cost_delete);
+    }
+    if (i > 0 && dp[i][j].cost == dp[i - 1][j].cost + cost_delete) {
+        find_all_paths(dp, A, B, i - 1, j, current + 'D', all_paths, cost_replace, cost_insert, cost_delete);
+    }
+}
+
+void apply_operations(const string& A, const string& B, const string& operations) {
+    string result = A;
+    int i = 0, j = 0; 
+
+   
+    cout << result << endl;
+
+    for (char op : operations) {
+        if (op == 'M') {
+            ++i;
+            ++j;
+        }
+        else if (op == 'R') {
+            result[i] = B[j];
+            ++i;
+            ++j;
+            cout << result << endl;
+        }
+        else if (op == 'I') {
+            result.insert(result.begin() + i, B[j]);
+            ++i;
+            ++j;
+            cout << result << endl;
+        }
+        else if (op == 'D') {
+            result.erase(result.begin() + i);
+            cout << result << endl;
+        }
+    }
+}
+
+int main() {
+    setlocale(LC_ALL, "Russian");
+    int cost_replace, cost_insert, cost_delete;
+    string A, B;
+
+    cin >> cost_replace >> cost_insert >> cost_delete;
+    cin >> A >> B;
+
+    auto dp = build_dp(A, B, cost_replace, cost_insert, cost_delete);
+
+    vector<string> all_paths;
+    find_all_paths(dp, A, B, A.length(), B.length(), "", all_paths, cost_replace, cost_insert, cost_delete);
+
+    cout << "Все возможные изменения:" << endl;
+    for (const string& path : all_paths)
+        cout << path << endl;
+
+    if (!all_paths.empty()) {
+        cout << "Пример редакционного предписания:" << endl;
+        apply_operations(A, B, all_paths[0]);
+    }
+
+    return 0;
+}
+*/
+
+
+vector<vector<Cell>> build_dp(const string& A, const string& B, int cost_replace, int cost_insert, int cost_delete) {
+    int m = A.length();
+    int n = B.length();
+    vector<vector<Cell>> dp(m + 1, vector<Cell>(n + 1));
+
+    dp[0][0] = { 0, 'M' };
+    for (int i = 1; i <= m; ++i)
+        dp[i][0] = { dp[i - 1][0].cost + cost_delete, 'D' };
+    for (int j = 1; j <= n; ++j)
+        dp[0][j] = { dp[0][j - 1].cost + cost_insert, 'I' };
+
+    for (int i = 1; i <= m; ++i) {
+        for (int j = 1; j <= n; ++j) {
+            if (A[i - 1] == B[j - 1]) {
+                dp[i][j] = { dp[i - 1][j - 1].cost, 'M' };
+            }
+            else {
+                int replace_cost = dp[i - 1][j - 1].cost + cost_replace;
+                int insert_cost = dp[i][j - 1].cost + cost_insert;
+                int delete_cost = dp[i - 1][j].cost + cost_delete;
+
+                if (replace_cost <= insert_cost && replace_cost <= delete_cost) {
+                    dp[i][j] = { replace_cost, 'R' };
+                }
+                else if (insert_cost <= replace_cost && insert_cost <= delete_cost) {
+                    dp[i][j] = { insert_cost, 'I' };
+                }
+                else {
+                    dp[i][j] = { delete_cost, 'D' };
+                }
+            }
+        }
+    }
+
+    return dp;
+}
+
+
+
+
+
+void print_dp_table(const vector<vector<Cell>>& dp, const string& A, const string& B) {
+    cout << "\nТаблица стоимости преобразований (dp):\n    ";
+    for (char ch : B) cout << "     " << ch;
+    cout << "\n  ";
+    for (int j = 0; j <= B.length(); ++j) {
+        cout << "[" << dp[0][j].cost << "," << dp[0][j].operation << "]";
+    }
+    cout << endl;
+
+    for (int i = 1; i <= A.length(); ++i) {
+        cout << A[i - 1] << " ";
+        for (int j = 0; j <= B.length(); ++j) {
+            cout << "[" << dp[i][j].cost << "," << dp[i][j].operation << "]";
+        }
+        cout << endl;
+    }
+}
+
+void find_all_paths(const vector<vector<Cell>>& dp, const string& A, const string& B,
+    int i, int j, string current, vector<string>& all_paths,
+    int cost_replace, int cost_insert, int cost_delete) {
+    if (i == 0 && j == 0) {
+        reverse(current.begin(), current.end());
+        all_paths.push_back(current);
+        return;
+    }
+
+    if (i > 0 && j > 0) {
+        if (A[i - 1] == B[j - 1] && dp[i][j].cost == dp[i - 1][j - 1].cost) {
+            find_all_paths(dp, A, B, i - 1, j - 1, current + 'M', all_paths, cost_replace, cost_insert, cost_delete);
+        }
+        if (A[i - 1] != B[j - 1] && dp[i][j].cost == dp[i - 1][j - 1].cost + cost_replace) {
+            find_all_paths(dp, A, B, i - 1, j - 1, current + 'R', all_paths, cost_replace, cost_insert, cost_delete);
+        }
+    }
+    if (j > 0 && dp[i][j].cost == dp[i][j - 1].cost + cost_insert) {
+        find_all_paths(dp, A, B, i, j - 1, current + 'I', all_paths, cost_replace, cost_insert, cost_delete);
+    }
+    if (i > 0 && dp[i][j].cost == dp[i - 1][j].cost + cost_delete) {
+        find_all_paths(dp, A, B, i - 1, j, current + 'D', all_paths, cost_replace, cost_insert, cost_delete);
+    }
+}
+
+void apply_operations(const string& A, const string& B, const string& operations) {
+    string result = A;
+    int i = 0, j = 0;
+
+    cout << "\nПошаговое применение операций:\n";
+    cout << "Начальное слово: " << result << endl;
+
+    for (char op : operations) {
+        if (op == 'M') {
+            cout << "Match (совпадение): " << result[i] << " == " << B[j] << " (позиция " << i << ")\n";
+            ++i; ++j;
+        }
+        else if (op == 'R') {
+            cout << "Replace (замена): " << result[i] << " -> " << B[j] << " (позиция " << i << ")\n";
+            result[i] = B[j];
+            ++i; ++j;
+            cout << "Результат: " << result << endl;
+        }
+        else if (op == 'I') {
+            cout << "Insert (вставка): " << B[j] << " на позицию " << i << "\n";
+            result.insert(result.begin() + i, B[j]);
+            ++i; ++j;
+            cout << "Результат: " << result << endl;
+        }
+        else if (op == 'D') {
+            cout << "Delete (удаление): " << result[i] << " с позиции " << i << "\n";
+            result.erase(result.begin() + i);
+            cout << "Результат: " << result << endl;
+        }
+    }
+}
+
+int main() {
+    setlocale(LC_ALL, "Russian");
+    int cost_replace, cost_insert, cost_delete;
+    string A, B;
+
+    cout << "Введите стоимость замены, вставки и удаления: ";
+    cin >> cost_replace >> cost_insert >> cost_delete;
+    cout << "Введите исходную строку A: ";
+    cin >> A;
+    cout << "Введите целевую строку B: ";
+    cin >> B;
+
+    auto dp = build_dp(A, B, cost_replace, cost_insert, cost_delete);
+    print_dp_table(dp, A, B);
+
+    vector<string> all_paths;
+    find_all_paths(dp, A, B, A.length(), B.length(), "", all_paths, cost_replace, cost_insert, cost_delete);
+
+    cout << "\nВсе возможные редакционные пути (операции):" << endl;
+    for (const string& path : all_paths)
+        cout << path << endl;
+
+    if (!all_paths.empty()) {
+        cout << "\nПример применения операций для первой последовательности:\n";
+        apply_operations(A, B, all_paths[0]);
+    }
+
+    return 0;
+}
